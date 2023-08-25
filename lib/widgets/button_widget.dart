f88e2f182d98ab0732f7f2963431450fd85d6cf1
Lans12345 <olanalans12345@gmail.com>
2023-08-25 18:32:39 +0800

    added initial project structure

---
+++ lib/widgets/button_widget.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:valley_mobile/widgets/text_widget.dart';
+
+class ButtonWidget extends StatelessWidget {
+  final String label;
+  final VoidCallback onPressed;
+  final double? width;
+  final double? fontSize;
+  final double? height;
+  final double? radius;
+  final Color? color;
+  final Color? fontColor;
+
+  const ButtonWidget(
+      {super.key,
+      required this.label,
+      required this.onPressed,
+      this.width = 300,
+      this.fontSize = 18,
+      this.height = 50,
+      this.radius = 5,
+      this.color = Colors.blue,
+      this.fontColor = Colors.white});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialButton(
+        shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(radius!)),
+        minWidth: width,
+        height: height,
+        color: color,
+        onPressed: onPressed,
+        child: TextBold(
+          text: label,
+          fontSize: fontSize!,
+          color: fontColor!,
+        ));
+  }
+}
