3ef8f8d310732217495a39abccc7e03d82309398
Lans12345 <olanalans12345@gmail.com>
2023-08-25 18:43:09 +0800

    added main screens

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,10 +1,89 @@
 import 'package:flutter/material.dart';
+import 'package:valley_mobile/widgets/text_widget.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            GestureDetector(
+              onTap: () {},
+              child: Container(
+                height: 200,
+                width: 250,
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.blue,
+                  ),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Icon(
+                        Icons.app_registration,
+                        color: Colors.blue,
+                        size: 75,
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      TextBold(
+                        text: 'Register NFC',
+                        fontSize: 24,
+                        color: Colors.blue,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 50,
+            ),
+            GestureDetector(
+              onTap: () {},
+              child: Container(
+                height: 200,
+                width: 250,
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.blue,
+                  ),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Icon(
+                        Icons.qr_code_scanner_sharp,
+                        color: Colors.blue,
+                        size: 75,
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      TextBold(
+                        text: 'Scan NFC',
+                        fontSize: 24,
+                        color: Colors.blue,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
