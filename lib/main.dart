3af1dad1d032515b35f288b4fcb5662f5d887f86
Lans12345 <olanalans12345@gmail.com>
2023-08-26 10:09:56 +0800

    added firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:valley_mobile/screens/home_screen.dart';
 
+import 'firebase_options.dart';
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
+  await Firebase.initializeApp(
+    name: 'valley-9b203',
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
